9cb3500ae2e142a19169daa4ae8913fdbcba49e6
Kieruyagi <82781271+Kieruyagi@users.noreply.github.com>
2025-12-06 04:01:08 -0600

    got the minigame working
    need to polish room and make items spawn when grabbed

--- Assets/CustomScripts/IngredientDetect.cs
+++ Assets/CustomScripts/IngredientDetect.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class IngredientDetect : MonoBehaviour
 {
+<<<<<<< Updated upstream
       public List<string> recipe = new List<string>()
     {
         "lettuce","cheese","tomato","patty","topBun","bottomBun","drink","fries"
@@ -12,6 +13,24 @@ public class IngredientDetect : MonoBehaviour
     private List<string> recipeCheck = new List<string>();
 
     private bool recipeCompleted = false;
+=======
+    private List<string> recipeList = new List<string>()
+    {
+        "topBun","bottomBun","patty","cheese","lettuce","tomato"
+    };
+    private List<string> recipe = new List<string>();
+    private List<string> recipeCheck = new List<string>();
+
+    public List<GameObject> ingredientsInside = new List<GameObject>();
+
+    public int totalOrders = 3;
+    private int recipesCompleted = 0;
+    private int randomNum = 0;
+
+    private int number = 0;
+
+    public AudioSource sucessAudio;
+>>>>>>> Stashed changes
 
     private void OnTriggerEnter(Collider other)
     {
@@ -23,6 +42,11 @@ public class IngredientDetect : MonoBehaviour
             ingredientsInside.Add(obj);
             if(!recipeCheck.Contains(obj.tag)) recipeCheck.Add(obj.tag);
             Debug.Log($"Added: {obj.name} | Count: {ingredientsInside.Count}");
+<<<<<<< Updated upstream
+=======
+            Debug.Log($"{randomNum} random num | {recipeCheck.Count} recipes count");
+            Debug.Log($"{recipesCompleted} recipes complete | {recipe.Count} recipes count");
+>>>>>>> Stashed changes
         }
     }
 
@@ -39,6 +63,7 @@ public class IngredientDetect : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
     private void Update()
     {
         if (CheckRecipe() && (!recipeCompleted))
@@ -60,11 +85,60 @@ public class IngredientDetect : MonoBehaviour
         {
             //Debug.Log("Recipe Not Complete!!!");
             return false;
+=======
+    private void Awake()
+    {
+        NewRecipe();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckRecipe();
+    }
+
+    public void CheckRecipe()
+    {
+        if((recipe.Count <= recipeCheck.Count) && (recipesCompleted < totalOrders))
+        {
+            recipesCompleted++;
+            Debug.Log($"{recipesCompleted} Recipes Complete!!!!");
+            OnRecipeCompleted();
+        }
+        else if(recipesCompleted >= totalOrders)
+        {
+            //End script
+            Debug.Log("Ending Fast Food Script");
+            if(recipesCompleted >= totalOrders) this.enabled = false;
+        }     
+    }
+
+    private void printNum()
+    {
+        number++;
+        Debug.Log($"{number}");
+    }
+
+    private void NewRecipe()
+    {
+        Debug.Log("AWAKEN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        randomNum = Random.Range(3,7);
+        Debug.Log($"{randomNum} random number");
+
+        recipe = new List<string>();
+        recipeCheck = new List<string>();
+
+        for (int i = 0 ; i < randomNum ; i++)
+        {
+            recipe.Add(recipeList[i]);
+            Debug.Log($"{recipeList[i]} ingredient");
+>>>>>>> Stashed changes
         }
     }
 
     private void OnRecipeCompleted()
     {
+<<<<<<< Updated upstream
         Debug.Log("Recipe continues!");
         return;
         //Debug.Log("Recipe Completed!");
@@ -86,4 +160,24 @@ public class IngredientDetect : MonoBehaviour
     {
         return ingredientsInside.Count;
     }
+=======
+        Debug.Log("Recipes continues!");
+        
+        //Play sound
+        sucessAudio.Play();
+        //Remove ingredients
+        foreach (GameObject obj in ingredientsInside)
+        {
+            Destroy(obj);
+        }
+        ingredientsInside = new List<GameObject>();
+
+        //Delay 1 sec
+        //New recipe
+        NewRecipe();
+        
+
+        // Send UnityEvent, etc.
+    }
+>>>>>>> Stashed changes
 }
